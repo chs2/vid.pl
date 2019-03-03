37714594a760e35e83cade655cfe07042c7cee90
chs <chs@iomelette.fr>
2019-03-03 17:37:11 +0100

    Fix Non-static method Exception::__construct() cannot be called statically

--- src/Exception/Statement.php
+++ src/Exception/Statement.php
@@ -5,7 +5,7 @@ use PDOStatement;
 
 class Statement extends \Exception {
 	public static function instance(PDOStatement $statement, \Exception $previous = null) {
-		parent::__construct(
+		return new self(
 			$statement -> errorInfo()[2],
 			$statement -> errorInfo()[1],
 			$previous
